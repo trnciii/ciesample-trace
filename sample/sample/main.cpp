62a2a360c6b2d3f88d1e27f97ce8d7570a5c69ec
trnciii <doityan223@gmail.com>
2020-10-10 03:02:45 +0900

    add file

---
+++ sample/sample/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sample
+//
+//  Created by Kohei Doi on 2020/10/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
