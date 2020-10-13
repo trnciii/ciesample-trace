512f5d797729a0895811377042562e1e482104a8
trnciii <doityan223@gmail.com>
2020-10-14 01:38:54 +0900

    first commit as 3_box

---
+++ src/box.hpp
@@ -0,0 +1,11 @@
+//
+//  box.hpp
+//  sample
+//
+//  Created by Kohei Doi on 2020/10/14.
+//
+
+#pragma once
+#include "GLFW/glfw3.h"
+
+void makebox(double width, double height, double length, GLenum type=GL_LINE_LOOP);
