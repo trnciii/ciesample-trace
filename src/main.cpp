0de22f0ed6fa6341ed68f2db49ba17e9cdc8c816
trnciii <doityan223@gmail.com>
2020-11-23 01:15:28 +0900

    initial commit as 11_input101

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,17 @@
 #include "GLFW/glfw3.h"
 #include <math.h>
 #include "box.hpp"
+#include <stdio.h>
 
-// 登場する関数
+// 登場する関数(いつもの)
 void myinit(GLFWwindow** window);
 void reshape(GLFWwindow* window, int w, int h);
 void display(void);
+// 入力を受け付ける関数
+void KeyFunc(GLFWwindow* window, int key, int scancode, int action, int mods);
+void MouseButtonFunc(GLFWwindow* window, int button, int action, int mods);
+void MousePosFunc(GLFWwindow* window, double x, double y);
+void MouseWheelFunc(GLFWwindow* window, double x, double y);
 
 //--初期化—------------------------------------------------------------------
 void myinit(GLFWwindow** window)
@@ -22,6 +28,7 @@ void myinit(GLFWwindow** window)
     glClearColor(0, 0, 0, 1); // 背景色の設定
     
     reshape(*window, w, h); // 視点の初期化をおこなう
+    glEnable(GL_DEPTH_TEST);
 }
 
 
@@ -34,23 +41,71 @@ void reshape(GLFWwindow* window, int w, int h)
    
     gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
     gluLookAt(
-        3.0, 4.0, 5.0,    //どこから見てるか
+        0.0, 4.0, 5.0,    //どこから見てるか
         0.0, 0.0, 0.0,    //どこを見てるか
         0.0, 1.0, 0.0    //どの向きが上向きか
     );
 }
 
-//--描画内容--------------------------------------------------------------------
-void display(void)
+//--キーボード関数--------------------------------------------------------------
+void KeyFunc(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    glClear(GL_COLOR_BUFFER_BIT);
+    // ↑
+    if( key == GLFW_KEY_UP && action == GLFW_PRESS ){
+        printf("↑\n");
+    }
+    
+    // ↓
+    if( key == GLFW_KEY_DOWN && action == GLFW_PRESS ){
+        printf("↓\n");
+    }
+    
+    // ←
+    if( key == GLFW_KEY_LEFT && action == GLFW_PRESS ){
+        printf("←\n");
+    }
+    
+    // →
+    if( key == GLFW_KEY_RIGHT && action == GLFW_PRESS ){
+        printf("→\n");
+    }
+    
+    // スペースキー
+    if( key == GLFW_KEY_SPACE && action == GLFW_PRESS ){
+        printf("SPACE\n");
+    }
+    
+    // Aキー
+    if( key == 'A' && action == GLFW_PRESS ){
+        printf( "A\n" );
+    }
+}
+
+//--マウスボタン関数------------------------------------------------------------
+void MouseButtonFunc(GLFWwindow* window, int button, int action, int mods)
+{
+}
+
+//--マウスの位置関数------------------------------------------------------------
+void MousePosFunc(GLFWwindow* window, double x, double y)
+{
+}
+
+//--マウスのホイール関数--------------------------------------------------------
+void MouseWheelFunc(GLFWwindow* window, double x, double y)
+{
+}
+
+
+//--描画内容--------------------------------------------------------------------
+void display(int frame)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    // box.cpp内の関数を使ってボックスを描画。
-    // パラメータは、makebox(ｘ方向の長さ, y方向の長さ, z方向の長さ)
-    makebox(1.0,1.0,1.0);
+    makebox(1.0, 1.0, 1.0, GL_POLYGON);
 }
 
 //--メイン関数------------------------------------------------------------------
@@ -62,12 +117,21 @@ int main(void)
     
 
     //コールバック関数の登録 (キーボードやマウスの動きを捕まえる)
-    glfwSetWindowSizeCallback(window, reshape);
+    glfwSetWindowSizeCallback(window, reshape);             // ウィンドウの大きさが変わると呼び出される
+    glfwSetKeyCallback(window, KeyFunc);                    // キーボードを触ると呼び出される
+    glfwSetMouseButtonCallback(window, MouseButtonFunc);    // マウスのボタンを押すと呼び出される
+    glfwSetCursorPosCallback(window, MousePosFunc);         // マウスを動かすと呼び出される
+    glfwSetScrollCallback(window, MouseWheelFunc);          // マウスホイールを動かすと呼び出される
     
     
     //イベント処理ループ
+    int frame = 0;
+    
     do{
-        display();
+        frame++;
+        if(frame>300) frame = 0;
+        
+        display(frame);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }while( !glfwWindowShouldClose(window) );//ウィンドウが表示されるかぎりループする
